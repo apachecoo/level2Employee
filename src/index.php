0f8a7673e2b7d828aebf888d778bcf631b015d2b
Albert Pacheco Ospitia <apachecoo110@gmail.com>
2024-03-08 14:17:38 -0500

    se elimina archivo

--- src/index.php
+++ src/index.php
@@ -1,10 +1,7 @@
 <?php
-
-include_once 'functions/employee.php';
-
-$actions = $_REQUEST;
+$input = $_REQUEST ?? [];
 echo "<pre>";
-print_r($actions);
+print_r($input);
 echo "</pre>";
 ?>
 
@@ -13,7 +10,7 @@ echo "</pre>";
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Level 1 employee</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <style>
@@ -34,9 +31,8 @@ echo "</pre>";
     <div class="card">
         <img src="public/img/perfil.png" class="card-img-top" alt="Foto del Empleado" id="employee-photo">
         <div class="card-body">
-            <h5 class="card-title" >Registro empleado</h5>
-            <form action="functions/employee.php" method="post">
-
+            <h5 class="card-title">Registro empleado</h5>
+            <form action="index.php" method="post">
                 <div class="form-group">
                     <label for="gender">Género:</label>
                     <select name="employee[gender]" id="gender" class="form-select">
@@ -58,13 +54,23 @@ echo "</pre>";
                     <input type="text" class="form-control" id="salary" name="employee[salary]">
                 </div>
                 <hr>
-                <button type="submit" class="btn btn-primary btn-custom-width" id="update-salary-btn" value="modifySalary" name="action">
+                <button
+                        type="submit"
+                        class="btn btn-primary btn-custom-width"
+                        id="update-salary-btn"
+                        value="modifySalary"
+                        name="action">
                     Modificar Salario
                 </button>
 
                 <div class="row">
                     <div class="col">
-                        <button class="btn btn-secondary btn-custom-width" id="calculate-btn" value="calculateAge" name="action">Calcular Edad</button>
+                        <button
+                                class="btn btn-secondary btn-custom-width"
+                                id="calculate-btn"
+                                value="calculateAge"
+                                name="action">Calcular Edad
+                        </button>
                     </div>
                     <div class="col">
                         <p>valor edad</p>
@@ -72,28 +78,46 @@ echo "</pre>";
                 </div>
                 <div class="row">
                     <div class="col">
-                        <button class="btn btn-secondary btn-custom-width" id="calculate-btn" value="calculateSeniority" name="action">Calcular Antigüedad</button>
+                        <button class="btn btn-secondary btn-custom-width"
+                                id="calculate-btn"
+                                name="action"
+                                value="calculateSeniority"
+                        >
+                            Calcular Antigüedad
+                        </button>
                     </div>
                     <div class="col">
-                        <p>valor antig üedad</p>
+                        <p>valor antigüedad</p>
                     </div>
                 </div>
 
                 <div class="row">
                     <div class="col">
-                        <button class="btn btn-secondary btn-custom-width" id="calculate-btn" value="calculateBenefits" name="action">Calcular Prestaciones</button>
+                        <button class="btn btn-secondary btn-custom-width"
+                                id="calculate-btn"
+                                value="calculateBenefits"
+                                name="action"
+                        >
+                            Calcular Prestaciones
+                        </button>
                     </div>
                     <div class="col">
-                        <p>calcular prestaciones</p>
+                        <p>prestaciones</p>
                     </div>
                 </div>
-                <button class="btn btn-info btn-custom-width" id="change-employee-btn">Cambiar Empleado</button>
-            </form>
 
+                <button
+                        class="btn btn-info btn-custom-width"
+                        id="change-employee-btn"
+                        name="action"
+                        value="changeEmployee"
+                >
+                    Cambiar Empleado
+                </button>
+            </form>
         </div>
     </div>
 </div>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
         crossorigin="anonymous"></script>
